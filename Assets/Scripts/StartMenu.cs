7eb69f8826eeebb6457d4b26661afe8dae76c5f6
zhanhaor <31212257+zhanhaor@users.noreply.github.com>
2020-08-04 22:03:47 -0700

    Add files via upload

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
